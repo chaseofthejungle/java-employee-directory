7d8f07403c6bc8d119520c01ddbb9bbe53716aee
Chase <127450469+chaseofthejungle@users.noreply.github.com>
2023-05-18 17:25:50 -0400

    Added Phone Number column to DB.

--- 01-thymeleaf-demo-employees-starter-list/sql-scripts/employee-directory.sql
+++ 01-thymeleaf-demo-employees-starter-list/sql-scripts/employee-directory.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE  IF NOT EXISTS `employee_directory`;
+CREATE DATABASE IF NOT EXISTS `employee_directory`;
 USE `employee_directory`;
 
 --
@@ -11,6 +11,7 @@ CREATE TABLE `employee` (
   `id` int NOT NULL AUTO_INCREMENT,
   `first_name` varchar(45) DEFAULT NULL,
   `last_name` varchar(45) DEFAULT NULL,
+  `phone_number` varchar(15) DEFAULT NULL,
   `email` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
@@ -20,9 +21,8 @@ CREATE TABLE `employee` (
 --
 
 INSERT INTO `employee` VALUES 
-	(1,'Leslie','Andrews','leslie@luv2code.com'),
-	(2,'Emma','Baumgarten','emma@luv2code.com'),
-	(3,'Avani','Gupta','avani@luv2code.com'),
-	(4,'Yuri','Petrov','yuri@luv2code.com'),
-	(5,'Juan','Vega','juan@luv2code.com');
-
+	(1,'Hank','Scorpio','(111) 711-5080','hank.scorpio@cotj.com'),
+	(2,'Pedro','Chespirito','(222) 444-9999','pedro.chespirito@cotj.com'),
+	(3,'Mai','Sharona','(333) 867-5309','mai.sharona@cotj.com'),
+	(4,'Nick','Foles','(444) 777-7777','nick.foles@cotj.com'),
+	(5,'Mario','Mario','(555) 555-5555','mario.mario@cotj.com');
---
+++ 01-thymeleaf-demo-employees-starter-list/src/test/java/com/cotj/springboot/employeedirectoryapp/EmployeeDirectoryAppTests.java
@@ -0,0 +1,13 @@
+package com.cotj.springboot.employeedirectoryapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeDirectoryAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
